63df29d1724463e4de96abb1d6232f62c04b5af2
Omid Ayoobe <oayoo001@gold.ac.uk>
2020-02-03 12:42:24 +0000

    json Unmarshal the event.deatil payload

--- health-dashboard-goLang/notifier/notifier.go
+++ health-dashboard-goLang/notifier/notifier.go
@@ -1,1 +1,32 @@
 package notifier
+
+import "encoding/json"
+
+type SlackMessage struct {
+	Text      string  `json:"text"`
+	IconEmoji string `json:"icon_emoji,omitempty"`
+}
+
+type Description struct {
+	LatestDescription string `json:"latestDescription"`
+}
+
+type AffectedEntity struct {
+	EntityValue string `json:"entityValue"`
+}
+
+type CloudwatchEvent struct {
+	EventType        string           `json:"eventTypeCode"`
+	Service          string           `json:"service"`
+	EventDescription []Description    `json:"eventDescription"`
+	AffectedEntities []AffectedEntity `json:"affectedEntities"`
+
+}
+
+func CloudWatchEventToMessage(source []byte) (*SlackMessage, error) {
+	var event CloudwatchEvent
+	if err := json.Unmarshal(source, &event); err != nil {
+		return nil, err
+	}
+	return err, nil
+}
--- health-dashboard-goLang/notifier/notifier_test.go
+++ health-dashboard-goLang/notifier/notifier_test.go
@@ -1,6 +1,8 @@
 package notifier
 
-import "testing"
+import (
+	"testing"
+	)
 
 const payload = `
 {
