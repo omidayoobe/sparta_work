cd3ba368a2f219c43e2cdc54b11bd90736117826
Omid Ayoobe <oayoo001@gold.ac.uk>
2020-02-25 17:12:44 +0000

    Calling the module to stop reusing code.

---
+++ health-dashboard-golang/dashboad.tf
@@ -0,0 +1,6 @@
+
+provider "aws" {
+  region = var.region
+  access_key = var.AWS_ACCESS_KEY_ID   # aws access key
+ secret_key = var.AWS_SECRET_ACCESS_KEY # aws secret key
+}
---
+++ health-dashboard-golang/variable.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  type = string
+  default = "eu-west-1"
+}
+variable "env" {
+  type = string
+}
+
+variable "AWS_ACCESS_KEY_ID"{
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY"{
+  type = string
+}
