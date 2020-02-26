11fced348648c3610d8ace3f094a0f2bad331c9d
Omid Ayoobe <oayoo001@gold.ac.uk>
2020-02-26 16:37:09 +0000

    hange name of the folder

---
+++ health-dashboard-notifier/dashboad.tf
@@ -0,0 +1,15 @@
+
+provider "aws" {
+  assume_role {
+    role_arn = "health-dashboard-warning: arn:aws:iam::687447591496:role/omidsRoleForMonitoring"
+  }
+  region = var.region
+  access_key = var.AWS_ACCESS_KEY_ID   # aws access key
+ secret_key = var.AWS_SECRET_ACCESS_KEY # aws secret key
+}
+
+module "health-dashboard-notifier" {
+  source = "../terraform-health-dashboard"
+  env = var.env
+  lambdas_folder = var.
+}
