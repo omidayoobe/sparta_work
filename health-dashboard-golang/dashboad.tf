3d894753213b0ecc578682539ff640e2f394e6d5
Omid Ayoobe <oayoo001@gold.ac.uk>
2020-02-26 13:35:45 +0000

    hiding variable file and extracting the module to use it.

--- health-dashboard-golang/dashboad.tf
+++ health-dashboard-golang/dashboad.tf
@@ -4,3 +4,8 @@ provider "aws" {
   access_key = var.AWS_ACCESS_KEY_ID   # aws access key
  secret_key = var.AWS_SECRET_ACCESS_KEY # aws secret key
 }
+
+module "health-dashboard-notifier" {
+  source = "https://github.com/omidayoobe/sparta_work/tree/master/terraform-health-dashboard/module"
+
+}
